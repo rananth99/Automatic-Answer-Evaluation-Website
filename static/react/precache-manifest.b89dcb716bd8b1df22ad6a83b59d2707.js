0e9ca56f4a40bf20147b1e451c209895d585e236
PythonProblem <vishwas3151999@gmail.com>
2020-05-25 14:17:46 +0530

    Cleanup

---
+++ static/react/precache-manifest.b89dcb716bd8b1df22ad6a83b59d2707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869c8ef2e6efb5574813783be3c9e520",
+    "url": "/static/react/../../../templates/index.html"
+  },
+  {
+    "revision": "614c9651ea73167fdc5e",
+    "url": "/static/react/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "31826b2eef8c0da4b36c",
+    "url": "/static/react/js/2.1ff6a04e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/react/js/2.1ff6a04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614c9651ea73167fdc5e",
+    "url": "/static/react/js/main.5154ac5a.chunk.js"
+  },
+  {
+    "revision": "c7241993fd2c7bed7213",
+    "url": "/static/react/js/runtime-main.b9f37f5d.js"
+  }
+]);
